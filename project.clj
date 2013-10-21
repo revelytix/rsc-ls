e653433bf2f25025c5bcd074673156782d8da671
Paul Gearon <gearon@computer.org>
2013-10-21 10:00:36 -0400

    Updated group name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rsc-ls "0.0.1-SNAPSHOT"
+(defproject org.clojars.quoll/rsc-ls "0.0.1-SNAPSHOT"
   :description "Resource listing plugin for Leiningen"
   :url "https://github.com/revelytix/rsc-ls"
   :license {:name "Eclipse Public License"
