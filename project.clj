186ce02b256f4ae581d608251f83fc24f5ae402e
Paul Gearon <gearon@computer.org>
2013-10-21 09:55:43 -0400

    Added project URL

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject rsc-ls "0.0.1-SNAPSHOT"
   :description "Resource listing plugin for Leiningen"
+  :url "https://github.com/revelytix/rsc-ls"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
